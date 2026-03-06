025afd2b9bda0f5bc1873424c1fbe8247d6dbf20
aloksahhh <aloksah048@gmail.com>
2026-03-06 08:48:47 +0530

    Update page.tsx in app section

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -7,8 +7,20 @@ export default function AboutPage() {
     
    <main className="w-full py-16 bg-gray-100">
       <section className="max-w-7xl mx-auto px-6 ">
-        <ResearchCard />
-           <ResearchCard />
+        <ResearchCard
+  title="Artificial Intelligence Research"
+  description="Research in machine learning, deep learning and intelligent systems."
+  imageSrc="/img/IT.jpeg"
+  imageAlt="AI Research"
+/>
+
+<ResearchCard
+  title="Data Science Research"
+  description="Research in big data analytics and predictive modelling."
+  imageSrc="/img/IT1.jpeg"
+  imageAlt="Data Science"
+/>
+
        </section>
        </main>
        
